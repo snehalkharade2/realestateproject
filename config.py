7787942263a17f034ac0964eb3b9a799de950ae6
snehalkharade2 <kharadesnehal2@gmail.com>
2023-02-20 17:56:21 +0530

    Add config file

---
+++ config.py
@@ -0,0 +1,9 @@
+
+
+KNN_REG_FILE_PATH = r"artifacts/knn_regression.pkl"
+
+NORMALIZED_FILE_PATH = r"artifacts/minmaxscaler.pkl"
+
+JSON_FILE_PATH = r"artifacts/project_data.json"
+
+PORT_NUMBER = 5011
